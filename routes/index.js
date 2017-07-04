1011453b4162ad9a763b9bd8a896b8329f80941b
Cameron Massey <cameron_massey@yahoo.com>
2017-07-04 12:30:57 -0700

    Started the website

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cameron Massey' });
+});
+
+router.get('/resume', function(req, res, next) {
+  res.render('resume', { title: 'Cameron Massey' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Cameron Massey' });
+});
+
+module.exports = router;
