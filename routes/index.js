2108af86eda3e0379a08d2647870902b9b9ccc94
Cameron Massey <cameron_massey@yahoo.com>
2018-04-08 11:26:24 -0800

    Removed some excess images
    Removed excess images and changed links to match the actual url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('welcome_page', { title: 'Cameron Massey' });
+  res.render('fancy_home_page', { title: 'Cameron Massey' });
 });
 
 router.get('/resume', function(req, res, next) {
